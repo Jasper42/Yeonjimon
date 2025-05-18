794b743197f5d454c24d3426649516c9774104e8
Jass <tkjasper42@gmail.com>
2025-05-19 02:52:33 +0500

    added build.bat

---
+++ build.bat
@@ -0,0 +1,7 @@
+@echo off
+
+echo Building the bot with new updates...
+npm run build
+echo Bot built successfully!
+
+pause
--- update.bat
+++ update.bat
@@ -1,21 +1,11 @@
 @echo off
 
-echo ========================================
 echo Updating the bot from GitHub...
-echo ========================================
 git pull origin main
 echo Bot updated successfully!
 
-echo ========================================
 echo Installing dependencies...
-echo ========================================
 npm install
 echo Dependencies installed successfully!
-
-echo ========================================
-echo Building the bot with new updates...
-echo ========================================
-npm run build
-echo Bot built successfully!
 
 pause
